dc7eb40c14f4e5ddd1521e00ed87b398aaf243e7
Marco Bellezza <jasone.mb@outlook.com>
2024-12-23 18:59:50 +0100

    add datestring check

--- modules/date.ts
+++ modules/date.ts
@@ -1,2 +1,14 @@
 export const zero = new Date(0);
 export const now = () => new Date();
+
+export const isDateString = (dateString: string) => {
+  const regex = /^\d{4}-\d{2}-\d{2}$/;
+  if (!regex.test(dateString)) return false;
+
+  const [day, month, year] = dateString.split('-').map(Number);
+  const date = new Date(year, month - 1, day);
+
+  return date.getFullYear() === year &&
+    date.getMonth() === month - 1 &&
+    date.getDate() === day;
+};
